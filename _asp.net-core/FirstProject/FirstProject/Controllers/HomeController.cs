6cdb210b1e7b871f18e4608e934f8b7c86cee6d0
unknown <s.adeboye@outlook.com>
2023-05-29 23:43:23 +0100

    added rsvpform page

--- _asp.net-core/FirstProject/FirstProject/Controllers/HomeController.cs
+++ _asp.net-core/FirstProject/FirstProject/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        //public ViewResult Index()
+        //{
+        //    int hour = DateTime.Now.Hour;
+        //    string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        //    return View("MyView", viewModel);
+        //}
+        public IActionResult Index()
         {
-            int hour = DateTime.Now.Hour;
-            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
-            return View("MyView", viewModel);
+            return View();
+        }
+        public ViewResult RsvpForm()
+        {
+            return View();
         }
     }
 }
