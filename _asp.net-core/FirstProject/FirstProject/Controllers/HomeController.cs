9bcb6b5cc20e4255ca935a38f2b53ae2e37cddc5
unknown <s.adeboye@outlook.com>
2023-05-30 23:58:47 +0100

    updated the homecontroller

--- _asp.net-core/FirstProject/FirstProject/Controllers/HomeController.cs
+++ _asp.net-core/FirstProject/FirstProject/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace FirstProject.Controllers
             Repository.AddResponse(guestResponse);
             return View("Thanks", guestResponse);
         }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
